ebebadcd00a1d2a953a7e266d0d243d9fd536ba9
San-Goon <tigerlove95@naver.com>
2022-11-26 20:39:46 +0900

    fix: wrap app with provider

--- App.tsx
+++ App.tsx
@@ -1,51 +1,14 @@
-import {NavigationContainer} from '@react-navigation/native';
-import Settings from './src/pages/Settings';
-import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Info from './src/pages/Info';
-import Home from './src/pages/Home';
-import Investment from './src/pages/Investment';
+import * as React from 'react';
+import {Provider} from 'react-redux';
+import store from './src/store';
+import InnerApp from './InnerApp';
 
-const App = () => {
-  const Tab = createBottomTabNavigator();
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       const data = await axios.get(
-  //         'https://test.daground.io/info/contents',
-  //         // { headers: {'TEST-AUTH': 'sandbankfrontend'}, }
-  //       );
-  //       console.log(data);
-  //     } catch (error) {
-  //       console.error(error.response);
-  //     }
-  //   };
-  //   getData();
-  // }, []);
-
+function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="홈" component={Home} options={{headerShown: false}} />
-        <Tab.Screen
-          name="투자"
-          component={Investment}
-          options={{headerShown: false}}
-        />
-        <Tab.Screen
-          name="인포"
-          component={Info}
-          options={{headerShown: false}}
-        />
-        <Tab.Screen
-          name="설정"
-          component={Settings}
-          options={{headerShown: false}}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <InnerApp />
+    </Provider>
   );
-};
+}
 
 export default App;
---
+++ InnerApp.tsx
@@ -0,0 +1,36 @@
+import {NavigationContainer} from '@react-navigation/native';
+import Settings from './src/pages/Settings';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Info from './src/pages/Info';
+import Home from './src/pages/Home';
+import Investment from './src/pages/Investment';
+
+const App = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="홈" component={Home} options={{headerShown: false}} />
+        <Tab.Screen
+          name="투자"
+          component={Investment}
+          options={{headerShown: false}}
+        />
+        <Tab.Screen
+          name="인포"
+          component={Info}
+          options={{headerShown: false}}
+        />
+        <Tab.Screen
+          name="설정"
+          component={Settings}
+          options={{headerShown: false}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
