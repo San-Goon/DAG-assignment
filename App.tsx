055d8b3a42f066f83ee56a5fb2d9239672193bab
San-Goon <tigerlove95@naver.com>
2022-11-26 19:28:34 +0900

    feat: make structure in Info

--- App.tsx
+++ App.tsx
@@ -6,35 +6,31 @@ import Info from './src/pages/Info';
 import Home from './src/pages/Home';
 import Investment from './src/pages/Investment';
 
-function AppInner() {
+const App = () => {
   const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="홈" component={Home} options={{headerShown: false}} />
         <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Tab.Screen
-          name="Investment"
+          name="투자"
           component={Investment}
           options={{headerShown: false}}
         />
         <Tab.Screen
-          name="Info"
+          name="인포"
           component={Info}
           options={{headerShown: false}}
         />
         <Tab.Screen
-          name="Settings"
+          name="설정"
           component={Settings}
           options={{headerShown: false}}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-export default AppInner;
+export default App;
--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import BJob from './BJob';
+import Youtube from './Youtube';
+import Insight from './Insight';
+import Quiz from './Quiz';
+import Market from './Market';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 const Info = () => {
+  const Tab = createMaterialTopTabNavigator();
   return (
-    <View>
-      <Text>Info</Text>
-    </View>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="알쓸B잡" component={BJob} />
+      <Tab.Screen name="유튜브" component={Youtube} />
+      <Tab.Screen name="인사이트" component={Insight} />
+      <Tab.Screen name="퀴즈" component={Quiz} />
+      <Tab.Screen name="마켓" component={Market} />
+    </Tab.Navigator>
   );
 };
 
